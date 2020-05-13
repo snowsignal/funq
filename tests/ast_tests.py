af91c087d485e4b3e73bf5f819f702dc15fde708
Jackson Lewis <inquisitivepenguin@protonmail.com>
2020-05-13 13:54:53 +0100

    Add groundwork for unit tests

---
+++ tests/ast_tests.py
@@ -0,0 +1,18 @@
+import unittest
+from ast_builder import ASTBuilder
+from input_parser import parse
+
+
+class TestASTBuild(unittest.TestCase):
+    def setUp(self) -> None:
+        self.contents_for_test = """
+        region Test<5> { Q[] q1 = ^000^; }
+        """
+    def test_ast_builds_correctly(self):
+        tree = parse(self.contents_for_test)
+        # Perform verification on the tree
+        self.assertTrue(len(tree.children) == 1)
+        ast = ASTBuilder(tree).ast
+
+if __name__ == "__main__":
+    unittest.main()
